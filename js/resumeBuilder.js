0c2edad9c420d713f9291aa15e6b5513016fc294
kunalrshah <kunal.rajnikant.shah@gmail.com>
2015-03-15 18:40:39 -0400

    Commit initializeResume() - function I decided to add by myself. Not mentioned by instructors or the project. displayMyWorkHistory() projects display() encapsulation completed
    google map not yet working

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,22 +1,9 @@
-var myName = "Kunal R Shah";
-var myEmail = "kunal.rajnikant.shah@gmail.com";
-var formattedName = HTMLheaderName.replace("%data%", myName);
-
-
-var myRole = "Senior Solution Architect";
-var formatterRole = HTMLheaderRole.replace("%data%", myRole);
-
-var formattedEmail = HTMLemail.replace("%data%", myEmail);
-
-
-$("#header").prepend(formatterRole);
-$("#header").prepend(formattedName);
 
 
 
 var myBio = {
 	"name" : {
-		"fistName" : "Kunal",
+		"firstName" : "Kunal",
 		"middleInitial" : "R",
 		"lastName" : "Shah"
 	},
@@ -25,8 +12,7 @@ var myBio = {
 		"emailId" : "kunal.rajnikant.shah@gmail.com",
 		"twitter" : "@kunalrshah",
 		"github" : "kunalrshah",
-		"city" : "Hillsborough",
-		"state" : "New Jersey"
+		"location" : "Hillsborougn, NJ, US"
 	},
 	"role" : "Senior Solution Architect / Senior Technology Manager",	
 	"bioPicUrl" : "images/kunalrshah.jpg",
@@ -34,31 +20,20 @@ var myBio = {
 	"welcomeMsg" : "Welcome To My Work Experience & Skills Summary"
 }
 
-    if (myBio.skills.length > 0)
-    {
-      $("#header").append(HTMLskillsStart);
 
-      var formattedSkill = "";
-      var skillsLen = myBio.skills.length;
-      for (i = 0; i < skillsLen; i++)
-      {
-        formattedSkill = HTMLskills.replace("%data%", myBio.skills[i]);  
-        $("#skills").append(formattedSkill);
-      }
-    }
 
 var myEducation = {
 	"schools" : [
 		{
 			"name" : "New Jersey Institute of Technology",
-			"city" : "Newark, NJ, US",
+			"location" : "Newark, NJ, US",
 			"degree" : "MS",
 			"major" : ["Computer Science"],
 			"graduationYear" : 1993
 		},
 		{
 			"name" : "Thadomal Shahani Engineering Collect, The university of Bombay",
-			"city" : "Mumbai, MH, India",
+			"location" : "Mumbai, MH, India",
 			"degree" : "BE",
 			"major" : ["Computer Engineering"],
 			"graduationYear" : 1989
@@ -78,6 +53,81 @@ var myEducation = {
 	]
 }
 
+var myProjects = {
+	"projects" : [
+		{
+			"name" : "Cross-Functional Architecture across Compliance, Finance and Risk Management technology divisions",
+			"designation" : "SVP / Senior Solution Architect - BigData Implementation for BI/Analytics",
+			"dates" : "2015",
+			"description" : "Cross-Functional Architecture across Compliance Tech, Finance Tech, Risk Management Tech and HR Tech. Some notable projects are with Data Innovation Office - on Data Governance Automation; Enterprise Analytics Platform, and Reusable Enterprise Assets Program",
+			"images" : ["images/XfuncArch.jpg"],
+		},
+		{
+			"name" : "HR Transformation / Global Manager Portal",
+			"dates" : "2013-2015",
+			"designation" : "SVP / Senior Technology Manager - Global HR Portal",
+			"description" : "Tier-0 Self Service Capability for Global Population of People Managers",
+			"images" : ["images/HRPortal.jpg"],
+		},
+		{
+			"name" : "Portfolio Architecture Services for Risk Management Technology across all banking businesses",
+			"dates" : "2010-2013",
+			"designation" : "SVP / Sr. Portfolio Architect",
+			"description" : "Provide end-to-end solution architecture services to $150MM portfolio of Risk Management Technology applications. Manage a small team of data architects, technical architects, as well as infrastructure architects",
+			"images" : ["images/riskmgmt.jpg"],
+		},
+		{
+			"name" : "eDelivery - Global Shared Utility (GSU) for Push eStatements",
+			"dates" : "2008-2010",
+			"designation" : "VP / Sr. Solutions Architect",
+			"description" : "For an enterprise-wide green initiative, implement a global utility service for various business units to delegate their Push eStatement capability tothe  eDelivery GSU",
+			"images" : ["images/estmt.jpg"],
+		},
+		{
+			"name" : "Shared Technology Services for Business Process Management Systems",
+			"dates" : "2006-2008",
+			"designation" : "VP / Global Engineering Lead - BPMS",
+			"description" : "Lead and managed a shared services organization / center of excellence for adoption & implementation of Business Process Management practices and technologies",
+			"images" : ["images/ste.jpg"],
+		}, 
+		{
+			"name" : "IT Consulting Engagement with COGNOS IT",
+			"dates" : "2005",
+			"designation" : "Senior Enterprise Integration Architect",
+			"description" : "Formulated a long-term strategy to support rapid growth, improve data quality and consistency and ensure interoperability of Cognos IT systems.",
+			"images" : ["images/cognos.png"],
+		},			
+		{
+			"name" : "IT Consulting / Staff Augmentation Engagement with Morgan Stanley",
+			"dates" : "1999-2005",
+			"designation" : "Senior Principal Consultant / Application Delivery Manager / Lead Developer",
+			"description" : "Re-Engineer an in-house built C++ system for  Acquisition and Distribution for Equity Derivatives Market Data - to a multi-tiered, message-oriented Pure Java solution",
+			"images" : ["images/mktdata.jpg"],
+		}
+	],
+	"display" : function() {
+
+		var proj = {};
+
+		for (proj in myProjects.projects)
+		{
+			$("#projects").append(HTMLprojectStart);
+
+			var formattedProjTitle = HTMLprojectTitle.replace("%data%", myProjects.projects[proj].name);
+			var formattedProjDates = HTMLprojectDates.replace("%data%", myProjects.projects[proj].dates);
+			var formattedProjDescr = HTMLprojectDescription.replace("%data%", myProjects.projects[proj].description);
+			var formattedProjRec = formattedProjTitle + formattedProjDates + formattedProjDescr;
+			$(".project-entry:last").append(formattedProjRec);
+
+
+			for (img in myProjects.projects[proj].images) {
+				var formattedProjImage = HTMLprojectImage.replace("%data%", myProjects.projects[proj].images[img]);
+				$(".project-entry:last").append(formattedProjImage);
+			}
+		}
+	}
+}
+
 var myWorkHistory = {
 	"myJobs" : [
 		{
@@ -86,33 +136,6 @@ var myWorkHistory = {
 			"dates" : "02/2006-Present",
 			"location" : "Warren, NJ, US",
 			"jobDescription" : "Responsibilities include variety of roles ranging from Product Engineering, Solution Architecture, Management of Application Delivery, and Massively Parallel Processing for Analytical Models",
-			"projects" : [
-				{
-					"name" : "Cross-Functional Architecture across Compliance, Finance and Risk Management technology divisions",
-					"designation" : "SVP / Senior Solution Architect - BigData Implementation for BI/Analytics",
-					"description" : "Cross-Functional Architecture across Compliance Tech, Finance Tech, Risk Management Tech and HR Tech. Some notable projects are with Data Innovation Office - on Data Governance Automation; Enterprise Analytics Platform, and Reusable Enterprise Assets Program"
-				},
-				{
-					"name" : "HR Transformation / Global Manager Portal",
-					"designation" : "SVP / Senior Technology Manager - Global HR Portal",
-					"description" : "Tier-0 Self Service Capability for Global Population of People Managers"
-				},
-				{
-					"name" : "Portfolio Architecture Services for Risk Management Technology across all banking businesses",
-					"designation" : "SVP / Sr. Portfolio Architect",
-					"description" : "Provide end-to-end solution architecture services to $150MM portfolio of Risk Management Technology applications. Manage a small team of data architects, technical architects, as well as infrastructure architects"
-				},
-				{
-					"name" : "eDelivery - Global Shared Utility (GSU) for Push eStatements",
-					"designation" : "VP / Sr. Solutions Architect",
-					"description" : "For an enterprise-wide green initiative, implement a global utility service for various business units to delegate their Push eStatement capability tothe  eDelivery GSU"
-				},
-				{
-					"name" : "Shared Technology Services for Business Process Management Systems",
-					"designation" : "VP / Global Engineering Lead - BPMS",
-					"description" : "Lead and managed a shared services organization / center of excellence for adoption & implementation of Business Process Management practices and technologies"
-				}
-			],
 			"isCurrentJob" : 1
 		},
 		{
@@ -121,13 +144,6 @@ var myWorkHistory = {
 			"dates" : "06/2005-01/2006",
 			"location" : "New York, NY, US",
 			"jobDescription" : "Consulting Integration Architect for a short-term (sdix months) engagement with COGNOS IT; guiding them in their efforts to rationalize their IT application portfolio and help them re-engineer their systems interactions to utilize a centralized Customer Data Hub and enfore Master Data Management standards.",
-			"projects" : [
-				{
-					"name" : "IT Consulting Engagement with COGNOS IT",
-					"designation" : "Senior Enterprise Integration Architect",
-					"description" : "Formulated a long-term strategy to support rapid growth, improve data quality and consistency and ensure interoperability of Cognos IT systems."
-				}
-			],
 			"isCurrentJob" : 0
 		},
 		{
@@ -136,37 +152,86 @@ var myWorkHistory = {
 			"dates" : "08/1999-06/2005",
 			"location" : "Princeton Junction, NJ, US",
 			"jobDescription" : "Manage, Lead and Develop the next generation solution for capturing real-time market data from various stock exchanges as well as reuters; and establish a publish/subscribe mechanism to distribute the same to downstream systems for their timely utilization.",
-			"projects" : [
-				{
-					"name" : "IT Consulting / Staff Augmentation Engagement with Morgan Stanley",
-					"designation" : "Senior Principal Consultant / Application Delivery Manager / Lead Developer",
-					"description" : "Re-Engineer an in-house built C++ system for  Acquisition and Distribution for Equity Derivatives Market Data - to a multi-tiered, message-oriented Pure Java solution"
-				}
-			],
 			"isCurrentJob" : 0
 		}
 	]
 }
 
 
-
-
-console.log(myWorkHistory.myJobs.length);
-
-var job = {};
-
-for (job in myWorkHistory.myJobs)
+function initializeResume()
 {
-	$("#workExperience").append(HTMLworkStart);
-	var formattedEmployer = HTMLworkEmployer.replace("%data%", myWorkHistory.myJobs[job].employer);
-	var formattedTitle = HTMLworkTitle.replace("%data%", myWorkHistory.myJobs[job].position);
-	var formattedWorkDates = HTMLworkDates.replace("%data%", myWorkHistory.myJobs[job].dates);
-	var formattedWorkDescr = HTMLworkDescription.replace("%data%", myWorkHistory.myJobs[job].jobDescription);
-	var formattedJobRec = formattedEmployer + formattedTitle + formattedWorkDates + formattedWorkDescr;
+	//var myName = myBio.name.firstName + " " + myBio.name.lastName;
+	var myName = inName(myBio.name.firstName.trim(), myBio.name.lastName.trim());
 
-	$(".work-entry:last").append(formattedJobRec);
+	var myEmail = myBio.contactInfo.emailId;
+	var myRole = myBio.role;
+
+	var formattedName = HTMLheaderName.replace("%data%", myName);
+	var formatterRole = HTMLheaderRole.replace("%data%", myRole);
+	var formattedEmail = HTMLemail.replace("%data%", myEmail);
+
+
+	$("#header").prepend(formatterRole);
+	$("#header").prepend(formattedName);
+
+
+    if (myBio.skills.length > 0)
+    {
+      $("#header").append(HTMLskillsStart);
+
+      var formattedSkill = "";
+      var skillsLen = myBio.skills.length;
+      for (i = 0; i < skillsLen; i++)
+      {
+        formattedSkill = HTMLskills.replace("%data%", myBio.skills[i]);  
+        $("#skills").append(formattedSkill);
+      }
+    }
+}
+
+
+function displayMyWorkHistory() {
+
+	console.log(myWorkHistory.myJobs.length);
+
+	var job = {};
+
+	for (job in myWorkHistory.myJobs)
+	{
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", myWorkHistory.myJobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", myWorkHistory.myJobs[job].position);
+		var formattedWorkDates = HTMLworkDates.replace("%data%", myWorkHistory.myJobs[job].dates);
+		var formattedWorkDescr = HTMLworkDescription.replace("%data%", myWorkHistory.myJobs[job].jobDescription);
+		var formattedJobRec = formattedEmployer + formattedTitle + formattedWorkDates + formattedWorkDescr;
+
+		$(".work-entry:last").append(formattedJobRec);
+
+	}	
+
+	myProjects.display();
+
+	$("#mapDiv").append(googleMap);	
 
 }
+
+function inName(fName, lName)
+{
+	var fN = fName.charAt(0).toUpperCase() + fName.substr(1).toLowerCase();
+	var lN = lName.toUpperCase();
+
+	return (fN + " " + lN);
+}
+
+
+initializeResume();
+displayMyWorkHistory();
+
+$(document).click(function(loc) {
+	logClicks(loc.pageX, loc.pageY);
+});
+
+$("#main").append(internationalizeButton);
 
 // $("#main").append(myCurrentJob["myPosition"]);
 // $("#main").append(myEducation.mySchoolName);
