93f88b1cafeb63ab955d5710a65d5b6d36523c40
kunalrshah <kunal.rajnikant.shah@gmail.com>
2015-03-16 22:48:48 -0400

    Signed-off-by: kunalrshah <kunal.rajnikant.shah@gmail.com>

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -17,8 +17,40 @@ var myBio = {
 	"role" : "Senior Solution Architect / Senior Technology Manager",	
 	"bioPicUrl" : "images/kunalrshah.jpg",
 	"skills" : ["application development", "business process management", "project management", "architecture delivery"],
-	"welcomeMsg" : "Welcome To My Work History and Projects Summary"
-}
+	"welcomeMsg" : "Welcome To My Work History and Projects Summary",
+	"displayHeaderContact" : function(anchorString) {
+		var formattedMobileNum = HTMLmobile.replace("%data%", myBio.contactInfo.phoneNum);
+		var formattedEmail = HTMLemail.replace("%data%", myBio.contactInfo.emailId);
+		var formattedGitHub = HTMLgithub.replace("%data%", myBio.contactInfo.github);
+		var formattedTwitter = HTMLtwitter.replace("%data%", myBio.contactInfo.twitter);
+		var formattedLocation = HTMLlocation.replace("%data%", myBio.contactInfo.location);
+
+		//$("#topContacts").append(formattedMobileNum + formattedEmail + formattedGitHub + formattedTwitter + formattedLocation);
+
+		// for the Header - use anchorString as "#topContacts"
+		// for the footer - user anchorString as "#footerContacts"
+		$(anchorString).append(formattedMobileNum + formattedEmail + formattedGitHub + formattedTwitter + formattedLocation);
+	},
+	"displayHeaderBio" : function() {
+		var formattedBioPic = HTMLbioPic.replace("%data%", myBio.bioPicUrl);
+		var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", myBio.welcomeMsg);
+
+		$("#header").append(formattedBioPic + formattedWelcomeMsg);
+
+	    if (myBio.skills.length > 0)
+	    {
+	      $("#header").append(HTMLskillsStart);
+
+	      var formattedSkill = "";
+	      var skillsLen = myBio.skills.length;
+	      for (i = 0; i < skillsLen; i++)
+	      {
+	        formattedSkill = HTMLskills.replace("%data%", myBio.skills[i]);  
+	        $("#skills").append(formattedSkill);
+	      } // Iterate through the skills array and format each skill using the helper js
+	    } // check skills array length
+	} // display function
+} // myBio object
 
 
 
@@ -59,8 +91,87 @@ var myEducation = {
 			"courseUrl" : "https://www.coursera.org/specialization/jhudatascience/1?utm_medium=listingPage",
 			"dates" : "2015"
 		}
-	]
-}
+	],
+	"display" : function() {
+		var sch = {};
+		for (sch in myEducation.schools)
+		{
+			$("#education").append(HTMLschoolStart);
+			var formattedSchoolName = HTMLschoolName.replace("%data%", myEducation.schools[sch].name);
+			var formattedSchoolDeg = HTMLschoolDegree.replace("%data%", myEducation.schools[sch].degree);
+			var formattedSchoolDates = HTMLschoolDates.replace("%data%", myEducation.schools[sch].dates);
+			var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", myEducation.schools[sch].location);
+
+			$(".education-entry:last").append(formattedSchoolName + formattedSchoolDeg + formattedSchoolDates + formattedSchoolLocation);
+
+			var mj = {};
+			for (mj in myEducation.schools[sch].majors) {
+				var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", myEducation.schools[sch].majors[mj]);
+				$(".education-entry:last").append(formattedSchoolMajor);			
+			} // for loop for majors
+		} // for loop schools
+
+
+		//	$("#online-courses").append(HTMLonlineClasses); // this line is commented out. header-3 added to the index.html
+
+		var course = {};
+		for (course in myEducation.onlineCourses) {
+
+			$("#online-courses").append(HTMLonlineCourseStart);		
+			var formattedCourseTitle = HTMLonlineTitle.replace("%data%", myEducation.onlineCourses[course].name);
+			var formattedCourseProvider = HTMLonlineSchool.replace("%data%", myEducation.onlineCourses[course].provider);
+			var formattedCourseDates = HTMLonlineDates.replace("%data%", myEducation.onlineCourses[course].dates);
+			var formattedCourseUrl = HTMLonlineURL.replace("%data%", myEducation.onlineCourses[course].courseUrl);
+
+			$(".course-entry:last").append(formattedCourseTitle + formattedCourseProvider + formattedCourseDates + formattedCourseUrl);
+		} // for loop - onlineClasses
+		$("#mapDiv").append(googleMap);	// Add map markers for education locations as well as work locations
+	} // end display function
+} // end myEducation
+
+var myWorkHistory = {
+	"myJobs" : [
+		{
+			"employer" : "Citigroup, Inc",
+			"position" : "Senior Solution Architect / Technology Manager",
+			"dates" : "02/2006-Present",
+			"location" : "Warren, NJ, US",
+			"jobDescription" : "Responsibilities include variety of roles ranging from Product Engineering, Solution Architecture, Management of Application Delivery, and Massively Parallel Processing for Analytical Models",
+			"isCurrentJob" : 1
+		},
+		{
+			"employer" : "GENPACT",
+			"position" : "Senior Enterprise Integration Architect (Consulting)",
+			"dates" : "06/2005-01/2006",
+			"location" : "New York, NY, US",
+			"jobDescription" : "Consulting Integration Architect for a short-term (sdix months) engagement with COGNOS IT; guiding them in their efforts to rationalize their IT application portfolio and help them re-engineer their systems interactions to utilize a centralized Customer Data Hub and enfore Master Data Management standards.",
+			"isCurrentJob" : 0
+		},
+		{
+			"employer" : "KEANE",
+			"position" : "Senior Principal Consultant",
+			"dates" : "08/1999-06/2005",
+			"location" : "Princeton Junction, NJ, US",
+			"jobDescription" : "Manage, Lead and Develop the next generation solution for capturing real-time market data from various stock exchanges as well as reuters; and establish a publish/subscribe mechanism to distribute the same to downstream systems for their timely utilization.",
+			"isCurrentJob" : 0
+		}
+	],
+	"display" : function() {
+		var job = {};
+		for (job in myWorkHistory.myJobs)
+		{
+			$("#workExperience").append(HTMLworkStart);
+			var formattedEmployer = HTMLworkEmployer.replace("%data%", myWorkHistory.myJobs[job].employer);
+			var formattedTitle = HTMLworkTitle.replace("%data%", myWorkHistory.myJobs[job].position);
+			var formattedWorkDates = HTMLworkDates.replace("%data%", myWorkHistory.myJobs[job].dates);
+			var formattedWorkDescr = HTMLworkDescription.replace("%data%", myWorkHistory.myJobs[job].jobDescription);
+			var formattedJobRec = formattedEmployer + formattedTitle + formattedWorkDates + formattedWorkDescr;
+
+			$(".work-entry:last").append(formattedJobRec);
+		} // for loop to iterate through the Work History, Jobs
+		myProjects.display();
+	} // display function
+} // myWorkHistory
 
 var myProjects = {
 	"projects" : [
@@ -132,39 +243,10 @@ var myProjects = {
 			for (img in myProjects.projects[proj].images) {
 				var formattedProjImage = HTMLprojectImage.replace("%data%", myProjects.projects[proj].images[img]);
 				$(".project-entry:last").append(formattedProjImage);
-			}
-		}
-	}
-}
-
-var myWorkHistory = {
-	"myJobs" : [
-		{
-			"employer" : "Citigroup, Inc",
-			"position" : "Senior Solution Architect / Technology Manager",
-			"dates" : "02/2006-Present",
-			"location" : "Warren, NJ, US",
-			"jobDescription" : "Responsibilities include variety of roles ranging from Product Engineering, Solution Architecture, Management of Application Delivery, and Massively Parallel Processing for Analytical Models",
-			"isCurrentJob" : 1
-		},
-		{
-			"employer" : "GENPACT",
-			"position" : "Senior Enterprise Integration Architect (Consulting)",
-			"dates" : "06/2005-01/2006",
-			"location" : "New York, NY, US",
-			"jobDescription" : "Consulting Integration Architect for a short-term (sdix months) engagement with COGNOS IT; guiding them in their efforts to rationalize their IT application portfolio and help them re-engineer their systems interactions to utilize a centralized Customer Data Hub and enfore Master Data Management standards.",
-			"isCurrentJob" : 0
-		},
-		{
-			"employer" : "KEANE",
-			"position" : "Senior Principal Consultant",
-			"dates" : "08/1999-06/2005",
-			"location" : "Princeton Junction, NJ, US",
-			"jobDescription" : "Manage, Lead and Develop the next generation solution for capturing real-time market data from various stock exchanges as well as reuters; and establish a publish/subscribe mechanism to distribute the same to downstream systems for their timely utilization.",
-			"isCurrentJob" : 0
-		}
-	]
-}
+			} // iterate thru' project images
+		} // iterate thru' all the projects to be included on the resume
+	} //display function
+} // myProjects
 
 
 function initializeResume()
@@ -182,102 +264,22 @@ function initializeResume()
 
 	$("#header").prepend(formatterRole);
 	$("#header").prepend(formattedName);
-
-
-}
+} // header - initialize resume
 
 
 function displayMyContactInfo() {
-
-	//var formattedContactInfo = HTMLcontactGeneric.replace("%data$%", )
-
-	var formattedMobileNum = HTMLmobile.replace("%data%", myBio.contactInfo.phoneNum);
-	var formattedEmail = HTMLemail.replace("%data%", myBio.contactInfo.emailId);
-	var formattedGitHub = HTMLgithub.replace("%data%", myBio.contactInfo.github);
-	var formattedTwitter = HTMLtwitter.replace("%data%", myBio.contactInfo.twitter);
-	var formattedLocation = HTMLlocation.replace("%data%", myBio.contactInfo.location);
-
-	$("#topContacts").append(formattedMobileNum + formattedEmail + formattedGitHub + formattedTwitter + formattedLocation);
-
-	var formattedBioPic = HTMLbioPic.replace("%data%", myBio.bioPicUrl);
-	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", myBio.welcomeMsg);
-
-	$("#header").append(formattedBioPic + formattedWelcomeMsg);
-
-    if (myBio.skills.length > 0)
-    {
-      $("#header").append(HTMLskillsStart);
-
-      var formattedSkill = "";
-      var skillsLen = myBio.skills.length;
-      for (i = 0; i < skillsLen; i++)
-      {
-        formattedSkill = HTMLskills.replace("%data%", myBio.skills[i]);  
-        $("#skills").append(formattedSkill);
-      }
-    }
-
-}
+} // contact info
 
 function displayMyWorkHistory() {
-
-	//console.log(myWorkHistory.myJobs.length);
-
-	var job = {};
-
-	for (job in myWorkHistory.myJobs)
-	{
-		$("#workExperience").append(HTMLworkStart);
-		var formattedEmployer = HTMLworkEmployer.replace("%data%", myWorkHistory.myJobs[job].employer);
-		var formattedTitle = HTMLworkTitle.replace("%data%", myWorkHistory.myJobs[job].position);
-		var formattedWorkDates = HTMLworkDates.replace("%data%", myWorkHistory.myJobs[job].dates);
-		var formattedWorkDescr = HTMLworkDescription.replace("%data%", myWorkHistory.myJobs[job].jobDescription);
-		var formattedJobRec = formattedEmployer + formattedTitle + formattedWorkDates + formattedWorkDescr;
-
-		$(".work-entry:last").append(formattedJobRec);
-
-	}	
-
-	myProjects.display();
 }
 
 function displayMyEducation() {
-
-	for (sch in myEducation.schools)
-	{
-		$("#education").append(HTMLschoolStart);
-		var formattedSchoolName = HTMLschoolName.replace("%data%", myEducation.schools[sch].name);
-		var formattedSchoolDeg = HTMLschoolDegree.replace("%data%", myEducation.schools[sch].degree);
-		var formattedSchoolDates = HTMLschoolDates.replace("%data%", myEducation.schools[sch].dates);
-		var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", myEducation.schools[sch].location);
-
-		$(".education-entry:last").append(formattedSchoolName + formattedSchoolDeg + formattedSchoolDates + formattedSchoolLocation);
-
-		for (mj in myEducation.schools[sch].majors) {
-			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", myEducation.schools[sch].majors[mj]);
-			$(".education-entry:last").append(formattedSchoolMajor);			
-		}
-	}
-
-
-//	$("#online-courses").append(HTMLonlineClasses);
-
-	for (course in myEducation.onlineCourses) {
-
-		$("#online-courses").append(HTMLonlineCourseStart);		
-		var formattedCourseTitle = HTMLonlineTitle.replace("%data%", myEducation.onlineCourses[course].name);
-		var formattedCourseProvider = HTMLonlineSchool.replace("%data%", myEducation.onlineCourses[course].provider);
-		var formattedCourseDates = HTMLonlineDates.replace("%data%", myEducation.onlineCourses[course].dates);
-		var formattedCourseUrl = HTMLonlineURL.replace("%data%", myEducation.onlineCourses[course].courseUrl);
-
-		//$(".education-entry:last").append(formattedCourseTitle + formattedCourseProvider + formattedCourseDates + formattedCourseUrl);
-		$(".course-entry:last").append(formattedCourseTitle + formattedCourseProvider + formattedCourseDates + formattedCourseUrl);
-	}
-
-	$("#mapDiv").append(googleMap);
-	// $("#main").append(internationalizeButton);
 }
 
+//
+// This function is meant to take the First Name and Last Name; and covert it to an internationalized form
+// e.g. "kUNal Shah" will be converted to  "Kunal SHAH"
+//
 function inName(fName, lName)
 {
 	var fN = fName.charAt(0).toUpperCase() + fName.substr(1).toLowerCase();
@@ -288,11 +290,18 @@ function inName(fName, lName)
 
 
 initializeResume();
-displayMyContactInfo();
-displayMyWorkHistory();
-displayMyEducation();
 
+// displayMyContactInfo();
+// displayMyWorkHistory();
+// displayMyEducation();
 
+myBio.displayHeaderContact("#topContacts");
+myBio.displayHeaderBio();
+myWorkHistory.display();
+myEducation.display();
+myBio.displayHeaderContact("#footerContacts");
+
+// $("#main").append(internationalizeButton);
 
 $(document).click(function(loc) {
 	logClicks(loc.pageX, loc.pageY);
